e54e4990f4ad937b64e39df0808a19cd090cdc0a
JaisinhBhosale9712 <71847281+JaisinhBhosale9712@users.noreply.github.com>
2022-02-07 01:35:23 +0100

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -12,7 +12,11 @@ def div(n):
 def main(n):
     res=fact(n)
     print(res)
+__all__ = ["unknown_func"]
   
 if __name__=="__main__":
     if len(sys.argv)>1:
         main(int(sys.argv[1]))
+def send_signal(pid, sig, pgid):
+    os.kill(pid, sig)  # Sensitive
+    os.killpg(pgid, sig)  # Sensitive
